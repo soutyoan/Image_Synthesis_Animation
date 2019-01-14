38d9c3f77cad7e82642e86cec2c7c9597cc3f886
Segretin Louis <louis.segretin@gmail.com>
2019-01-14 16:55:34 +0100

    Structure sans keyframes défini

--- common/node.py
+++ common/node.py
@@ -1,6 +1,8 @@
 """
 Hierarchy model of the class
 """
+import maya.cmds as cm
+
 
 GLOBAL_TAB_COUNT = 0
 
@@ -212,6 +214,17 @@ class Node:
 				assert len(i) == 0
 
 			return Root
+
+	def create_jointsAnimation_MAYA(self, nb_keyFrames):
+		"""
+		Creates the joints structure in MAYA
+		"""
+		cm.joint( name=self.name, p=self.position[0], o=self.rotation[0], roo="zyx", zso=True, oj='zyx', r=True )
+		for child in self.fils:
+			child.create_jointsAnimation_MAYA(nb_keyFrames)
+			cm.select( d=True )
+			
+		
 
 	def __str__(self):
 		global GLOBAL_TAB_COUNT
