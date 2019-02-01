e637f1cd87e0193e7bb5022499177bf13d88898d
antoninklopp <antonin.klopp-tosser@grenoble-inp.org>
2019-02-01 12:11:51 +0100

    Added list of names

--- viewer/test/test_bvh_creation.cpp
+++ viewer/test/test_bvh_creation.cpp
@@ -3,6 +3,9 @@
 int main(){
 
    Joint::createFromFile("viewer/models/walk.bvh");
+   for (int i = 0; i < Joint::list_names.size(); i++){
+     cout << "name " << Joint::findIndexOfJoint(Joint::list_names[i]) << " " << Joint::list_names[i] << endl;
+   }
 
     return 0;
 }
